44a90a6d9a195d93e3de18dd09094d96f8477a3e
JosefStonon <josef.sartori@outlook.com>
2023-04-19 20:52:48 -0300

    class components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 import { Container } from './styles';
 
-export default function Header() {
+export default function Header({ onToggleTheme, selectedTheme }) {
   return (
     <Container>
-      <h1>JStack's Blog</h1>
-      <button type="button">🌞</button>
+      <h1>Styled-Components</h1>
+      <button 
+        type="button"
+        onClick={onToggleTheme}
+      >
+        {selectedTheme === 'dark' ? '😎' : '🌛'}
+        
+      </button>
     </Container>
   );
 }
